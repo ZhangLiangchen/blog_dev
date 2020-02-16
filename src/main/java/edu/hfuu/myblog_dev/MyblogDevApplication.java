05cd3b8773b1771b2051e73e6d97174de4cd1a63
CHENG,Xi <Zhang_Liangchen@outlook.com>
2020-02-16 20:23:32 +0800

    项目初始化

---
+++ src/main/java/edu/hfuu/myblog_dev/MyblogDevApplication.java
@@ -0,0 +1,13 @@
+package edu.hfuu.myblog_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hfuu/myblog_dev/MyblogDevApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hfuu.myblog_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
