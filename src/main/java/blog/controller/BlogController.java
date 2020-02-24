2c484f18bc6095834c502c271fa4efa6c3b50e34
Ciel-08 <738675373@qq.com>
2020-02-24 20:42:39 +0800

    layui更新到最新版

---
+++ src/main/java/blog/controller/BlogController.java
@@ -0,0 +1,21 @@
+package blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Ciel-08
+ * @date 2020/2/24 19:52
+ */
+@Controller
+@RequestMapping("/admin")
+public class BlogController {
+
+    @GetMapping("/blogs")
+    public String listBlog() {
+
+        return "admin/blogs";
+    }
+
+}
--- src/main/java/blog/controller/TestController.java
+++
@@ -1,8 +0,0 @@
-package blog.controller;
-
-/**
- * @author Ciel-08
- * @date 2020/2/24 16:00
- */
-public class TestController {
-}
