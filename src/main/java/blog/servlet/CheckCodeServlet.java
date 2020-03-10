6c8623be75c900d7a3528f5a6176ff249580a8ec
Ciel-08 <738675373@qq.com>
2020-03-10 23:22:12 +0800

    [忽略]

--- src/main/java/blog/servlet/CheckCodeServlet.java
+++ src/main/java/blog/servlet/CheckCodeServlet.java
@@ -82,7 +82,6 @@ public class CheckCodeServlet extends HttpServlet {
 	 */
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
