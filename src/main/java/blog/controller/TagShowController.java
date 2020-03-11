1b3746c005270dff63b71e5cff315649f53ebdb2
xiboqi <38134665+xiboqi@users.noreply.github.com>
2020-03-12 07:50:44 +0800

    tagShow控制器和前端tag页面

--- src/main/java/blog/controller/TagShowController.java
+++ src/main/java/blog/controller/TagShowController.java
@@ -7,6 +7,7 @@ import blog.service.BlogService;
 import blog.service.TagService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,5 +23,31 @@ import java.util.List;
 public class TagShowController {
 
 
+    @Autowired
+    private TagService tagService;
 
+    @Autowired
+    private BlogService blogService;
+
+    @GetMapping("/tag/{id}")
+    public String tag(@RequestParam(defaultValue = "1",value = "pageNum") Integer pageNum,
+                      @PathVariable Long id, Model model) {
+        List<Tag> tag = tagService.getAllTag();
+        //-1表示从首页导航点进来的
+        if (id == -1) {
+            id = tag.get(0).getId();
+        }
+        model.addAttribute("tag", tag);
+        List<FirstPageBlog> blogs = blogService.getByTagId(id);
+        /*for ( FirstPageBlog a:blogs) {
+            System.out.println(a.getTypeName());
+            System.out.println(a.getAvatar());
+            System.out.println(a.getDescription());
+        }*/
+        PageHelper.startPage(pageNum, 100);
+        PageInfo<FirstPageBlog> pageInfo = new PageInfo<>(blogs);
+        model.addAttribute("pageInfo", pageInfo);
+        model.addAttribute("activeTagId", id);
+        return "tag";
+    }
 }
