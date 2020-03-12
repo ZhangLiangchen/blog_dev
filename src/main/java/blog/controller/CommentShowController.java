1710ef73b0540d57958012e531aef0096032cb37
Ciel-08 <738675373@qq.com>
2020-03-12 18:19:33 +0800

    Create CommentShowController.java

---
+++ src/main/java/blog/controller/CommentShowController.java
@@ -0,0 +1,12 @@
+package blog.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+//博客评论
+@Controller
+public class CommentShowController {
+
+
+}
